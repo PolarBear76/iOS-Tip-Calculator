254d2a6839f1f6d65d6060b8021f886065934d55
Axel Kaliff <axel.kaliff@gmail.com>
2018-02-04 14:47:50 +0100

    Initial Commit

---
+++ paulSolt2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  paulSolt2
+//
+//  Created by Axel Kaliff on 2018-02-04.
+//  Copyright © 2018 Axel Kaliff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
